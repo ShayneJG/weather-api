1a4951be915d1c9c7dab3f8c339013af32a43929
Shayne Geilman <sgeilman@myune.edu.au>
2025-11-27 15:02:20 +0800

    Added basic functionality
    added some basic structure to get the app running. Does not currently make any calls to the backend but it runs and quits successfully.

---
+++ tray/main.py
@@ -0,0 +1,46 @@
+import requests
+from PIL import Image, ImageDraw, ImageFont
+import pystray
+# Hardcoded for now for testing purposes
+backend_location = "http://192.168.50.155:8000"
+
+def fetch_latest_weather():
+    """
+    Sends a GET request to the backend to retrieve the latest weather station report.
+    """
+    endpoint = "/data/latest"
+    response = requests.get(backend_location + endpoint)
+    return response.json()
+
+def create_icon(uv_value):
+    """
+    Creates an icon to be shown in the tray. Currently supports displaying the UV.
+
+    Generates a 64x64 image with the UV written to the centre. 
+    
+    :param uv_value: The reported UV from the latest weather station report.
+    
+    :return image: The created image object. 
+    """
+    image = Image.new('RGB', (64, 64), color='darkblue')
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("arial.ttf", 48)
+    draw.text((16, 12), str(uv_value), fill='white', font=font)
+    return image
+
+def create_menu(data):
+
+    return pystray.Menu(
+        pystray.MenuItem(f"Temperature: {data.get('tempf', '?')}°F", None),
+        pystray.MenuItem(f"Humidity: {data.get('humidity', '?')}%", None),
+        pystray.MenuItem("Quit", lambda: quit_app(icon))
+    )
+
+def quit_app(icon):
+    icon.stop()
+
+
+
+if __name__ == "__main__":
+    icon = pystray.Icon("weather", create_icon("-"), "Weather", create_menu({}))
+    icon.run()
