27db945abe8fc9401e2a606eaea01031eb7bb918
Shayne Geilman <sgeilman@myune.edu.au>
2025-11-27 15:05:34 +0800

    updated documentation

--- tray/main.py
+++ tray/main.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 import pystray
 # Hardcoded for now for testing purposes
 backend_location = "http://192.168.50.155:8000"
-
+#TODO: Translate farenheit to celsius
 def fetch_latest_weather():
     """
     Sends a GET request to the backend to retrieve the latest weather station report.
@@ -11,7 +11,7 @@ def fetch_latest_weather():
     endpoint = "/data/latest"
     response = requests.get(backend_location + endpoint)
     return response.json()
-
+    
 def create_icon(uv_value):
     """
     Creates an icon to be shown in the tray. Currently supports displaying the UV.
@@ -29,7 +29,14 @@ def create_icon(uv_value):
     return image
 
 def create_menu(data):
+    """
+    Creates the right-click menu for the tray. 
 
+    Has a series of MenuItems representing different data points parsed from 
+    the backend data.
+
+    :param data: Data object containing key:value pairs representing weather station data. 
+    """
     return pystray.Menu(
         pystray.MenuItem(f"Temperature: {data.get('tempf', '?')}°F", None),
         pystray.MenuItem(f"Humidity: {data.get('humidity', '?')}%", None),
@@ -37,10 +44,16 @@ def create_menu(data):
     )
 
 def quit_app(icon):
+    """
+    Terminates the application.
+    
+    :param icon: The icon representing the tray application.
+    """
     icon.stop()
 
 
 
 if __name__ == "__main__":
     icon = pystray.Icon("weather", create_icon("-"), "Weather", create_menu({}))
+    # icon.run() is currently blocking. Application runs until quit_app is called by clicking the Quit MenuItem.
     icon.run()
